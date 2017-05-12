f98eb12bc3de6ef9d87c9a9f3a767c64082a81fc
Ishan Jain <ishanjain28@gmail.com>
2017-05-12 21:11:00 +0530

    Completed Project

---
+++ example/example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"log"
+	"github.com/ishanjain28/phash"
+)
+
+func main() {
+	firstImagePath := ""
+	secondImagePath := ""
+
+	fmt.Printf("Enter First Image Path: ")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		firstImagePath = s
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		os.Exit(1)
+	}
+	fmt.Printf("Enter Second Image Path: ")
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		secondImagePath = s
+		break
+	}
+	if err := scanner.Err(); err != nil {
+		os.Exit(1)
+	}
+	percentSimilar, err := phash.CalculateSimilarity(firstImagePath, secondImagePath, 512)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	fmt.Printf("Images are about %.3f similar\n", percentSimilar)
+}
--- perceptual-hash.go
+++ perceptual-hash.go
@@ -1,4 +1,4 @@
-package perceptual_hash
+package phash
 
 import (
 	"image/jpeg"
@@ -8,9 +8,12 @@ import (
 	"image/color"
 	"math"
 	"github.com/nfnt/resize"
+	"strconv"
+	"strings"
+	"path"
 )
 
-func DecodeImage(img *os.File, imageExt string) (image.Image, error) {
+func decodeImage(img *os.File, imageExt string) (image.Image, error) {
 
 	var decodedImage image.Image
 	var err error
@@ -26,7 +29,7 @@ func DecodeImage(img *os.File, imageExt string) (image.Image, error) {
 	return decodedImage, nil
 }
 
-func ConvertToGrayscale(img image.Image) *image.Gray16 {
+func convertToGrayscale(img image.Image) *image.Gray16 {
 	imageX, imageY := img.Bounds().Max.X, img.Bounds().Max.Y
 	grayImage := image.NewGray16(image.Rectangle{image.Point{0, 0}, image.Point{imageX, imageY}})
 
@@ -43,45 +46,120 @@ func ConvertToGrayscale(img image.Image) *image.Gray16 {
 	return grayImage
 }
 
-func DownsizeImage(img image.Image) image.Image {
-	return resize.Resize(4, 4, img, resize.Bicubic)
+func downsizeImage(img image.Image, hashLength int) image.Image {
+	widthAndLength := uint(math.Ceil(math.Sqrt(float64(hashLength)/2.0)) + 1)
+	return resize.Resize(widthAndLength, widthAndLength, img, resize.Bicubic)
 }
 
-func CalculateRowHash(img image.Image) []int {
+func calculateRowHash(img image.Image) string {
 
 	imageX, imageY := img.Bounds().Max.X, img.Bounds().Max.Y
-	hash := []int{}
-	for j := 0; j < imageY; j++ {
-		for i := 0; i < imageX; i++ {
-			r0, _, _, _ := img.At(j, i).RGBA()
-			r1, _, _, _ := img.At(j+1, i).RGBA()
+	completeHash := [][]string{}
 
-			if r0 >= r1 {
-				hash = append(hash, 1)
-			} else {
-				hash = append(hash, 0)
+	for j := 0; j < imageY-1; j++ {
+		rowHash := []string{}
+		for i := 0; i < imageX-1; i++ {
+			r0, _, _, _ := img.At(i, j).RGBA()
+			r1, _, _, _ := img.At(i+1, j).RGBA()
+			if r1 >= r0 {
+				rowHash = append(rowHash, strconv.Itoa(1))
+			}
+			if r1 < r0 {
+				rowHash = append(rowHash, strconv.Itoa(0))
 			}
 		}
+		completeHash = append(completeHash, rowHash)
 	}
+	completeHashString := []string{}
+
+	for i := 0; i < len(completeHash); i++ {
+		rowHashString := strings.Join(completeHash[i], "")
+		completeHashString = append(completeHashString, rowHashString)
+	}
+
+	return strings.Join(completeHashString, "")
+}
+
+func calculateColumnHash(img image.Image) string {
+	imageX, imageY := img.Bounds().Max.X, img.Bounds().Max.Y
+	completeHash := [][]string{}
+
+	for i := 0; i < imageX-1; i++ {
+		columnHash := []string{}
+		for j := 0; j < imageY-1; j++ {
+			r0, _, _, _ := img.At(i, j).RGBA()
+			r1, _, _, _ := img.At(i, j+1).RGBA()
+			if r1 >= r0 {
+				columnHash = append(columnHash, strconv.Itoa(1))
+			}
+			if r1 < r0 {
+				columnHash = append(columnHash, strconv.Itoa(0))
+			}
+		}
+		completeHash = append(completeHash, columnHash)
+	}
+
+	completeHashString := []string{}
+	for i := 0; i < len(completeHash); i++ {
+		columnHashString := strings.Join(completeHash[i], "")
+		completeHashString = append(completeHashString, columnHashString)
+	}
+
+	return strings.Join(completeHashString, "")
+}
+
+func CalculateHash(img image.Image) string {
+	rowHash := calculateRowHash(img)
+	columnHash := calculateColumnHash(img)
+	hash := ""
+	hash += rowHash
+	hash += columnHash
+
 	return hash
 }
 
-func CalculateColumnHash(img image.Image) []int {
-	imageX, imageY := img.Bounds().Max.X, img.Bounds().Max.Y
-	hash := []int{}
-
-	for i := 0; i < imageX; i++ {
-		for j := 0; j < imageY; j++ {
-			r0, _, _, _ := img.At(j, i).RGBA()
-			r1, _, _, _ := img.At(j, i+1).RGBA()
-
-			if r0 > r1 {
-				hash = append(hash, 1)
-			} else {
-				hash = append(hash, 0)
-			}
+func diffHashes(hash1, hash2 string, hashLength float32) float32 {
+	var similarityInTwoHashes float32
+	for i := 0; i < len(hash1); i++ {
+		if (byte(hash1[i]) ^ byte(hash2[i])) == 0 {
+			similarityInTwoHashes++
 		}
 	}
 
-	return hash
+	return (similarityInTwoHashes / hashLength) * 100
+}
+
+func loadFiles(firstImagePath, secondImagePath string) (*os.File, *os.File, error) {
+	firstImage, err := os.Open(firstImagePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	secondImage, err := os.Open(secondImagePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	return firstImage, secondImage, nil
+}
+
+func CalculateSimilarity(firstImagePath, secondImagePath string, hashLength int) (float32, error) {
+	firstImage, secondImage, err := loadFiles(firstImagePath, secondImagePath)
+	if err != nil {
+		return 0.0, err
+	}
+	defer firstImage.Close()
+	defer secondImage.Close()
+	firstDecodedImage, err := decodeImage(firstImage, path.Ext(firstImagePath))
+	if err != nil {
+		return 0.0, err
+	}
+	secondDecodedImage, err := decodeImage(secondImage, path.Ext(secondImagePath))
+	if err != nil {
+		return 0.0, err
+	}
+	firstGrayImage, secondGrayImage := convertToGrayscale(firstDecodedImage), convertToGrayscale(secondDecodedImage)
+
+	firstDownsizedImage, secondDownsizedImage := downsizeImage(firstGrayImage, hashLength), downsizeImage(secondGrayImage, hashLength)
+
+	return diffHashes(CalculateHash(firstDownsizedImage), CalculateHash(secondDownsizedImage), float32(hashLength)), nil
+
 }
